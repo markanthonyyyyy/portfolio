1120130d0b97851bd2df8e2641378d335f25b75f
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2024-08-29 10:28:44 +0800

    adjustments

--- src/Components/Screens/ContactScreen.tsx
+++ src/Components/Screens/ContactScreen.tsx
@@ -1,7 +1,7 @@
 
 const ContactScreen = () => {
   return (
-    <section id="Contact" className="text-white w-full h-full bg-zinc-800">
+    <section id="Connect" className="text-white w-full h-full bg-zinc-800">
       
 
     </section>
